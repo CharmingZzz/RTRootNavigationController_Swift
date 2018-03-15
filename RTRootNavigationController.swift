c7c2915efce0ac102a3bce2af28ae0fb28a41a6e
CharmingZzz <17600026862@163.com>
2018-03-15 18:14:29 +0800

    init

---
+++ RTRootNavigationController.swift
@@ -0,0 +1,12 @@
+//
+//  RTRootNavigationController.swift
+//  Pods-RTRootNavigationController_Swift_Example
+//
+//  Created by xujie on 2018/3/14.
+//
+
+import UIKit
+
+class RTRootNavigationController: NSObject {
+
+}
---
+++ RTRootNavigationController_Swift/Classes/UIViewController+RTRootNavigationController.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+RTRootNavigationController.swift
+//  Pods-RTRootNavigationController_Swift_Example
+//
+//  Created by xujie on 2018/3/15.
+//
+
+import Foundation
